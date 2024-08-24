3122768883a73c74de34151dcc311ba7a975029b
rahman3112 <rahman90143@gmail.com>
2024-08-24 01:44:08 -0700

    Your commit message here

---
+++ app/page.js
@@ -0,0 +1,24 @@
+'use client'
+import Login from "./login"
+import {getAuth, onAuthStateChanged} from "firebase/auth"
+import {app} from './config';
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+export default function Home() {
+   const router =useRouter();
+   const auth =getAuth(app);
+
+     useEffect(()=>{
+      onAuthStateChanged(auth,(user)=>{
+        if(user){
+          router.push('./dashboard');
+        }
+      });
+     },[auth, router]);
+  return (
+    <main className="flex min-h-screen flex-col items-center p-12">
+        <h1 className="text-4xl font-bold mb-10">Firebase OTP Sign-in</h1>
+        <Login />
+    </main>
+  );
+}
